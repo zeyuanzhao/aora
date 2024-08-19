63b7a4fa49046a76ad32e9af232d99f34e5c8767
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2024-08-19 17:57:27 -0400

    global context when signing in

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -8,9 +8,11 @@ import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
 
-import { signIn } from "../../lib/appwrite";
+import { getCurrentUser, signIn } from "../../lib/appwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const SignIn = () => {
+  const { setUser, setIsLoggedIn } = useGlobalContext();
   const [form, setForm] = useState({
     email: "",
     password: "",
@@ -26,9 +28,12 @@ const SignIn = () => {
     setIsSubmitting(true);
 
     try {
-      const result = await signIn(form.email, form.password);
+      await signIn(form.email, form.password);
 
-      // set it to global state...
+      const result = await getCurrentUser();
+
+      setUser(result);
+      setIsLoggedIn(true);
 
       router.replace("/home");
     } catch (error) {
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -9,8 +9,10 @@ import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
 
 import { createUser } from "../../lib/appwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const SignUp = () => {
+  const { setUser, setIsLoggedIn } = useGlobalContext();
   const [form, setForm] = useState({
     username: "",
     email: "",
@@ -29,7 +31,8 @@ const SignUp = () => {
     try {
       const result = await createUser(form.email, form.password, form.username);
 
-      // set it to global state...
+      setUser(result);
+      setIsLoggedIn(true);
 
       router.replace("/home");
     } catch (error) {
